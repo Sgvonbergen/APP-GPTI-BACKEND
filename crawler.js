da2aa7f747bb8ea5cb2c9df4899d402d4bd8c9b9
Sebasthian von Bergen <sgvonbergen@uc.cl>
2022-11-02 12:54:50 -0300

    added consorcio crawl

--- crawler.js
+++ crawler.js
@@ -4,18 +4,26 @@ const { Client } = require('pg');
 
 
 async function insertBancoConsorcio(client, data) {
-    let a = 1
+    await client.query('DELETE FROM orders WHERE bank_id = 2')
+    for(i = 0; i < data.length; i++) {
+        dat = data[i]
+        insert_query = `
+            INSERT INTO orders (bank_id, currency_id, interest_rate_online, interest_rate, min_time, max_time)
+            VALUES (${dat.bank_id}, ${dat.currency_id}, ${dat.interest_rate_online}, ${dat.interest_rate}, ${dat.min_time}, ${dat.max_time});
+            `
+        await client.query(insert_query)
+    }
 }
 
 
 async function insertBancoEstado(client, data) {
     // Insert Banco Estado Data
-    bank_q = "SELECT bank_id FROM banks WHERE bank_name = 'Banco Estado';"
-    bank_id = await (await client.query(bank_q)).rows[0].bank_id
-    currency_q = "SELECT currency_id FROM currencies WHERE currency_name = 'CLP';"
-    currency_id = await (await client.query(currency_q)).rows[0].currency_id
+    //bank_q = "SELECT bank_id FROM banks WHERE bank_name = 'Banco Estado';"
+    bank_id = 1
+    //currency_q = "SELECT currency_id FROM currencies WHERE currency_name = 'CLP';"
+    currency_id = 1
     // Delete previous orders from this bank
-    await client.query(`DELETE FROM orders WHERE bank_id = ${bank_id}`)
+    await client.query('DELETE FROM orders WHERE bank_id=1')
     for (let i = 0; i < data.length; i++) {
         row = data[i]
         if (typeof row !== 'undefined' && row.length > 0) {
@@ -53,7 +61,6 @@ async function crawlBancoEstado(browser) {
         waitUntil: 'domcontentloaded',
     })
     await page.waitForSelector('table[id=tasa-dep-plazo-pesos]')
-
     const result = await page.$$eval('#tasa-dep-plazo-pesos tr', rows => {
         return Array.from(rows, row => {
             const columns = row.querySelectorAll('td');
@@ -61,7 +68,6 @@ async function crawlBancoEstado(browser) {
         })
     })
     await page.close()
-    await browser.close()
     return result
 }
 
@@ -73,17 +79,58 @@ async function crawlBancoConsorcio(browser) {
     await page.goto(url, {
         waitUntil: 'domcontentloaded',
     })
-    await page.waitForSelector('ul[class=list]')
-    const dropdowns = await page.$$('ul[class=list]');
-    const typelist = await dropdowns[3].getProperty('parentNode')
-    await typelist.click()
-    let lis = await dropdowns[3].getProperty('childNode')
-    console.log(await (await lis.getProperty('className')).toString())
-    console.log(await (await typelist.getProperty('className')).toString())
+    await page.waitForSelector('div.nice-select')
+    await new Promise(resolve => setTimeout(resolve, 100))
+    const data = []
+    let dat;
+    let lists = await page.$$('div.nice-select')
+    const interest_rate_div = await page.$$('div.valores')
+    const interest_rate_list = await interest_rate_div[2].$('ul')
 
+    // Select deposit type
+    await lists[3].click()
+    let list = await lists[3].$('ul.list')
+    let option = await list.$('li[data-value="1"]')
+    await option.click()
+    await new Promise(resolve => setTimeout(resolve, 100))
+
+
+    // Select currency
+    lists = await page.$$('div.nice-select')
+    await lists[4].click()
+    list = await lists[4].$('ul.list')
+    option = await list.$('li[data-value="1"]')
+    await option.click()
+    await new Promise(resolve => setTimeout(resolve, 1000))
+
+
+    // Select time
+    lists = await page.$$('div.nice-select')
+    await lists[5].click()
+    list = await lists[5].$('ul.list')
+    options = await list.$$('li')
+    times = ['0', '7', '30', '60', '90', '180']
+    let op
+    for(i = 1; i < options.length - 1; i++) {
+        op = options[i]
+        await op.click()
+        await new Promise(resolve => setTimeout(resolve, 1000))
+        interest_rate_elem_text = await interest_rate_list.$eval('li#oTasaBaseMensual', el => el.innerText)
+        dat = {
+            bank_id: "2",
+            currency_id: "1",
+            min_time: times[i],
+            max_time: times[i],
+            interest_rate: interest_rate_elem_text.slice(2, -2),
+            interest_rate_online: interest_rate_elem_text.slice(2, -2),
+        }
+        data.push(dat)
+        await lists[5].click()
+    }
 
     await page.close()
-    await browser.close()
+
+    return data
 }
 
 
@@ -113,8 +160,9 @@ async function run(){
     await client.connect()
     data = await crawlBancoEstado(browser)
     await insertBancoEstado(client, data)
-    // data = await crawlBancoConsorcio(browser)
-    // await insertBancoConsorcio(data, client)
+    data = await crawlBancoConsorcio(browser)
+    await insertBancoConsorcio(client, data)
+    await browser.close()
 
     // close database connection
     await client.end()
